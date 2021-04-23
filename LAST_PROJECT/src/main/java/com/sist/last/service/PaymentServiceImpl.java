f3630d4353bf0222c166b5f7b9795e19267f9cbd
juok <juok9802@gmail.com>
2021-04-23 16:39:47 +0900

    payment, reply getPk

---
+++ LAST_PROJECT/src/main/java/com/sist/last/service/PaymentServiceImpl.java
@@ -0,0 +1,59 @@
+package com.sist.last.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sist.last.cmn.DTO;
+import com.sist.last.cmn.StringUtil;
+import com.sist.last.dao.PaymentDao;
+import com.sist.last.dao.PaymentDaoImpl;
+import com.sist.last.vo.Payment;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+	final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private PaymentDaoImpl dao;
+	
+	public PaymentServiceImpl() {}
+	
+	@Override
+	public List<?> doRetrieve(DTO dto) throws SQLException {
+		
+		return this.dao.doRetrieve(dto);
+	}
+
+	@Override
+	public int doUpdate(DTO dto) throws SQLException {
+	
+		return this.dao.doUpdate(dto);
+	}
+
+	@Override
+	public int doDelete(DTO dto) throws SQLException {
+		
+		return this.dao.doDelete(dto);
+	}
+
+	@Override
+	public int doInsert(DTO dto) throws SQLException {
+		Payment payment = (Payment) dto; 
+		payment.setPaySeq(StringUtil.getPK("")); 
+
+		return this.dao.doInsert(payment);
+	}
+
+	@Override
+	public DTO doSelectOne(DTO dto) throws SQLException {
+		
+		return this.dao.doSelectOne(dto);
+	}
+
+}
---
+++ LAST_PROJECT/src/main/java/com/sist/last/service/ReplyServiceImpl.java
@@ -0,0 +1,56 @@
+package com.sist.last.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.sist.last.cmn.DTO;
+import com.sist.last.cmn.StringUtil;
+import com.sist.last.dao.ReplyDao;
+import com.sist.last.vo.Reply;
+
+@Service
+public class ReplyServiceImpl implements ReplyService {
+
+	final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
+	private ReplyDao dao;
+	
+	public ReplyServiceImpl() {}
+	
+	@Override
+	public int doUpdate(DTO dto) throws SQLException {
+
+		return this.dao.doUpdate(dto);
+	}
+
+	@Override
+	public int doDelete(DTO dto) throws SQLException {
+
+		return this.dao.doDelete(dto);
+	}
+
+	@Override
+	public int doInsert(DTO dto) throws SQLException {
+		Reply reply = (Reply) dto;
+		reply.setReplySeq(StringUtil.getPK(""));
+		
+		return this.dao.doInsert(reply);
+	}
+
+	@Override
+	public DTO doSelectOne(DTO dto) throws SQLException {
+	
+		return this.dao.doSelectOne(dto);
+	}
+
+	@Override
+	public List<?> doRetrieve(DTO dto) throws SQLException {
+		
+		return this.dao.doRetrieve(dto);
+	}
+
+}
