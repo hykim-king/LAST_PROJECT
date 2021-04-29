ed3d2a5c5099ebee70c39753347cc0fa0d630aa2
juok <juok9802@gmail.com>
2021-04-29 12:01:56 +0900

    payment 뷰컨트롤러

--- LAST_PROJECT/src/main/java/com/sist/last/view/payment.java
+++ LAST_PROJECT/src/main/java/com/sist/last/view/payment.java
@@ -1,8 +1,27 @@
 package com.sist.last.view;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class payment {
+
+	//스토어 카테고리
+	@RequestMapping(value="store/store_category.do",method= RequestMethod.GET)
+	public String categoryView(Model model) throws SQLException {
+	
+		return "store/store_category";
+	}
+		
+	//스토어 홈
+	@RequestMapping(value="store/store_view.do",method= RequestMethod.GET)
+	public String homeView(Model model) throws SQLException {
+		
+		return "store/store_home";
+	}
 
 }
