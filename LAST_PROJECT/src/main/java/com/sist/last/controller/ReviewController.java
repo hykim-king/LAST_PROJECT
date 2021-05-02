0293039b002eaba7e9debbcf461a9cd2f028dec4
yeonsu <yeonsu8229@gmail.com>
2021-05-02 22:49:18 +0900

    review controller /리뷰 + 평점/ 등록, 삭제, 조회 commit

--- LAST_PROJECT/src/main/java/com/sist/last/controller/ReviewController.java
+++ LAST_PROJECT/src/main/java/com/sist/last/controller/ReviewController.java
@@ -1,7 +1,6 @@
 package com.sist.last.controller;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,17 +8,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
+import com.sist.last.cmn.Message;
 import com.sist.last.cmn.SearchReview;
 import com.sist.last.cmn.StringUtil;
-import com.sist.last.cmn.Message;
 import com.sist.last.service.ReviewService;
+import com.sist.last.service.StarServiceImpl;
 import com.sist.last.vo.Review;
+import com.sist.last.vo.ReviewStar;
+import com.sist.last.vo.Star;
 
 @Controller
 public class ReviewController {
@@ -29,12 +31,69 @@ public class ReviewController {
 
 	@Autowired
 	ReviewService reviewService;
+	
+	@Autowired
+	StarServiceImpl starService;
 
 	public ReviewController() {
 
 	}
 	
-	@RequestMapping(value = "review/do_retrieve.do", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "review/review_star_list.do", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String reviewStarList(SearchReview search) throws SQLException {
+
+		LOG.debug("----------------");
+		LOG.debug("param: " + search);
+		LOG.debug("----------------");
+
+		// review fk값 nvl 처리
+		search.setReviewFk(StringUtil.nvl(search.getReviewFk(), "-1"));
+
+		// 페이지 num
+		if (search.getPageNum() == 0) {
+			search.setPageNum(1);
+		}
+
+		// 페이지 size
+		if (search.getPageSize() == 0) {
+			search.setPageSize(10);
+		}
+
+		LOG.debug("----------------");
+		LOG.debug("param_init: " + search);
+		LOG.debug("----------------");
+
+		List<ReviewStar> list = (List<ReviewStar>) this.reviewService.reviewStarList(search);
+
+		for (ReviewStar vo : list) {
+			LOG.debug(vo.toString());
+		}
+		
+		
+		int totalCnt = 0;
+		if(list != null && list.size()>0) {
+			ReviewStar totalVO = list.get(0);
+			totalCnt = totalVO.getTotalReviews();
+		}
+			
+		ModelAndView model = new ModelAndView();
+		
+		model.addObject("list",list); 
+		model.addObject("totalCnt",totalCnt);
+		model.setViewName("store/Store_Detail_Info");
+		
+		// List to Json
+		Gson gson = new Gson();
+
+		String jsonList = gson.toJson(list);
+		LOG.debug("jsonList: " + jsonList);
+
+		return jsonList;
+
+	}
+	
+	@RequestMapping(value = "review/do_retrieve.do", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public String doRetrieve(SearchReview search) throws SQLException {
 
@@ -64,7 +123,7 @@ public class ReviewController {
 		for (Review vo : list) {
 			LOG.debug(vo.toString());
 		}
-
+		
 		// List to Json
 		Gson gson = new Gson();
 
@@ -133,13 +192,13 @@ public class ReviewController {
 
 	@RequestMapping(value = "review/do_delete.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public String doDelete(Review review) throws SQLException {
+	public String doDelete(Review review, Star star) throws SQLException {
 
 		LOG.debug("----------------");
 		LOG.debug("param: " + review);
 		LOG.debug("----------------");
 
-		int flag = this.reviewService.doDelete(review);
+		int flag = this.reviewService.doDelete(review, star);
 
 		String resultMsg = "";
 
@@ -166,14 +225,14 @@ public class ReviewController {
 
 	@RequestMapping(value = "review/do_insert.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public String doInsert(Review review) throws SQLException {
+	public String doInsert(Review review, Star star) throws SQLException {
 
 		LOG.debug("----------------");
 		LOG.debug("param: " + review);
 		LOG.debug("----------------");
-
-		int flag = reviewService.doInsert(review);
-
+		
+		int flag = reviewService.doInsert(review, star);
+		
 		String resultMsg = "";
 
 		if (1 == flag) {
