c776416bd9b5752e683a25a6b08e4dec34dd97b8
yeonsu <yeonsu8229@gmail.com>
2021-05-02 22:54:43 +0900

    스토어 단건조회, 장바구니 뷰컨트롤러 commit

--- LAST_PROJECT/src/main/java/com/sist/last/view/product.java
+++ LAST_PROJECT/src/main/java/com/sist/last/view/product.java
@@ -1,21 +1,44 @@
 package com.sist.last.view;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sist.last.cmn.SearchOrder;
+import com.sist.last.service.OptServiceImpl;
+import com.sist.last.service.ProductServiceImpl;
+import com.sist.last.vo.Opt;
+import com.sist.last.vo.Product;
 
 @Controller
 public class product {
 	
 	final Logger LOG = LoggerFactory.getLogger(product.class);
 	
+	@Autowired
+	ProductServiceImpl productService;
+	
+	@Autowired
+	OptServiceImpl optionService;
+	
 	@RequestMapping(value="store/store_detail.do",method= RequestMethod.GET)
-	public String productView(Model model) throws SQLException {
+	public String productView(Product product, Model model) throws SQLException {
+		
+		Product outVO = (Product) this.productService.doSelectOne(product);
+		
+		LOG.debug("==============");
+		LOG.debug("=outVO="+outVO);
+		LOG.debug("==============");
+		
+		model.addAttribute("vo", outVO);
 		
 		return "store/Store_Detail_Info";
 	}
@@ -26,4 +49,28 @@ public class product {
 		return "cmn/Member_Shopping_Basket";
 	}
 	
+	@RequestMapping(value="member/option_update.do",method= RequestMethod.GET)
+	public String optionUpdate(Model model) throws SQLException {
+		
+		return "cmn/Member_Shopping_Basket_Edit";
+	}
+	
+	@RequestMapping(value = "member/option_pop_up.do", method = RequestMethod.GET)
+    public String optionPopUp(Opt opt, Model model) throws SQLException {
+
+
+		opt.setDiv(1);
+		
+		
+		List<Opt> list = this.optionService.doRetrieve(opt);
+		
+		for(Opt vo : list) {
+			LOG.debug(vo.toString());
+		}
+		
+		model.addAttribute("list", list);
+		
+        return "cmn/Member_Shopping_Basket_Edit";
+    }
+
 }
